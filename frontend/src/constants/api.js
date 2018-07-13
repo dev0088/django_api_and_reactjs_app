f41f55abbc7333660a50ebdce0513a0a6be49157
ninjadev999 <mikenarkss@gmail.com>
2018-07-12 18:53:27 -0700

    pointed API gateway address

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,5 +1,5 @@
 export default {
-  url: "http://192.168.0.121:8000",
+  url: "http://127.0.0.1:8000",
 	formHeaders: {
 		'Content-Type': 'application/json, application/x-www-form-urlencoded; charset=UTF-8'
 	}
