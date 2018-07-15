5bc676e5c884b7d590f50f6ce829be6330194346
ninjadev999 <mikenarkss@gmail.com>
2018-07-15 11:09:57 -0700

    added swagger api interface on backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+	url(r'^$', schema_view),
     url(r'^admin/', admin.site.urls),
 	url(r'^api/v1/auth/', include('authentication.urls')),
 	url(r'^api/v1/shiptalent_info', include('shiptalent_info.urls')),
