17045231ab1b6717a3b053f712daab502d2008f0
ninjadev999 <mikenarkss@gmail.com>
2018-07-15 11:11:25 -0700

    updated API title

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='ShipTalent API')
 
 urlpatterns = [
 	url(r'^$', schema_view),
