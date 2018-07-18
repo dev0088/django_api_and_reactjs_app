4f92b8dc6163bf3fc7b443fca63b3c9dd8cf7f3f
ninjadev999 <mikenarkss@gmail.com>
2018-07-18 14:13:41 -0700

    changed static file path

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = 'static_backend'
 
 STATICFILES_DIRS = (
     ("js", os.path.join(STATIC_ROOT,'js')),
