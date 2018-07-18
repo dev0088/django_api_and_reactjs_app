25e0cfbdd1b0ce67bac30a4df6658593266d3202
ninjadev999 <mikenarkss@gmail.com>
2018-07-18 11:24:16 -0700

    updated apis url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,7 +21,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='ShipTalent API')
 
 urlpatterns = [
-	url(r'^$', schema_view),
+	url(r'^apis', schema_view),
     url(r'^admin/', admin.site.urls),
 	url(r'^api/v1/auth/', include('authentication.urls')),
 	url(r'^api/v1/shiptalent_info', include('shiptalent_info.urls')),
