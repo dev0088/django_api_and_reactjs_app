222263c4e14cc68c0cc7146c43abd566a320da73
ninjadev999 <mikenarkss@gmail.com>
2018-07-20 13:23:16 -0400

    Updated Video Interview page

--- frontend/src/containers/video-interview/practice.js
+++ frontend/src/containers/video-interview/practice.js
@@ -10,7 +10,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as videoActions from  '../../actions/videoActions';
 import AudioMeter from "../../components/audio-meter/index";
-import { questions } from "./constants";
 import DetectRTC from "detectrtc";
 
 import './styles.css';
@@ -25,9 +24,13 @@ class VideoPractice extends React.Component {
       config: true,
       currentQuestion: 0,
       isStopped: false,
+      isPlaying: false,
       isPlayBackOpen: false,
       alertOpen: false,
-      errors: []
+      errors: [],
+      waitingTime: [30, 120],
+      remainingTime: [30, 120],
+      timePos: 0,
     };
   }
   componentWillMount() {
@@ -58,29 +61,98 @@ class VideoPractice extends React.Component {
     this.props.videoActions.getVideoQuestionsActions();
     this.props.videoActions.getVideoSettingsActions();
   }
+  componentDidMount() {
+    this.countDown();
+  }
+  componentWillReceiveProps(nextProps) {
+    let { videoSettings } = nextProps;
+    let wait = [], remain = [];
+    if (videoSettings['value']['video_interview_prep_countdown'])
+      wait[0] = remain[0] = videoSettings['value']['video_interview_prep_countdown'];
+    else
+      wait[0] = remain[0] = 0;
+    if (videoSettings['value']['video_interview_response_time'])
+      wait[1] = remain[1] = videoSettings['value']['video_interview_response_time'];
+    else
+      wait[1] = remain[1] = 0;
+    this.setState({ waitingTime: wait, remainingTime: remain });
+  }
+  countDown = () => {
+    const { isStopped } = this.state;
+    const __this = this;
+    if (!isStopped)
+    {
+      setTimeout(function () {
+        const { remainingTime, timePos } = __this.state;
+        let isStopped1 = __this.state.isStopped;
+        if (!isStopped1)
+        {
+          if (remainingTime[timePos] === 0) {
+            if (timePos === 0) {
+              __this.setState({timePos: 1});
+            } else {
+              __this.setState({isStopped: true});
+            }
+          } else {
+            const newRemaining = [];
+            newRemaining[0] = remainingTime[0];
+            newRemaining[1] = remainingTime[1];
+            newRemaining[timePos]= remainingTime[timePos] - 1;
+            __this.setState({remainingTime: newRemaining});
+          }
+          __this.countDown();
+        }
+      }, 1000)
+    }
+  }
+
   onStopRecord = () => {
-    // console.log('onStopRecord');
-    this.setState({isStopped: true});
+    const { remainingTime } = this.state;
+    remainingTime[0] = remainingTime[1] = 0;
+    this.setState({isStopped: true, isPlaying: false});
+
   };
 
   onStartRecord = () => {
-    // console.log('onStartRecord');
-    this.setState({isStopped: false});
+    const { remainingTime } = this.state;
+    remainingTime[0] = 0;
+    this.setState({
+        isStopped: false, 
+        isPlaying: true, 
+        timePos: 1, 
+        remainingTime: remainingTime}, 
+      function() {
+        this.countDown();
+      });
   };
 
   onNextQuestion = () => {
+    const { remainingTime, waitingTime } = this.state;
+    remainingTime[0] = waitingTime[0];
+    remainingTime[1] = waitingTime[1];
     this.setState({
       isPlayBackOpen: false,
       currentQuestion: this.state.currentQuestion + 1,
-      isStopped: false
+      isStopped: false,
+      isPlaying: false,
+      timePos: 0
+    }, function() {
+      this.countDown();
     });
   };
 
   onBack = () => {
+    const { remainingTime, waitingTime } = this.state;
+    remainingTime[0] = waitingTime[0];
+    remainingTime[1] = waitingTime[1];
     this.setState({
       isPlayBackOpen: false,
       currentQuestion: 0,
-      isStopped: false
+      isStopped: false,
+      isPlaying: false,
+      timePos: 0
+    }, function() {
+      this.countDown();
     });
   };
 
@@ -94,8 +166,18 @@ class VideoPractice extends React.Component {
     window.location.reload();
   }
   render () {
-    const { config, errors, currentQuestion, isStopped, isPlayBackOpen } = this.state;
-    const { videoQuestions, videoSettings } = this.props;
+    const { 
+      config, 
+      errors, 
+      currentQuestion, 
+      isStopped,
+      isPlaying,
+      isPlayBackOpen, 
+      waitingTime, 
+      remainingTime,
+      timePos
+    } = this.state;
+    const { videoQuestions } = this.props;
     let question = "";
     const actions = [
       <FlatButton
@@ -113,55 +195,80 @@ class VideoPractice extends React.Component {
       question = videoQuestions.value[currentQuestion]['content'];
     return config ? (<div className="video-practice">
         <div className="video-interview-header">
-          <h1>My Video Interview (Cruise Staff)</h1>
-          <h3>Practice Questions</h3>
+          <h3>
+            <span className="pull-left">Question {currentQuestion + 1} of 5</span>
+            <span className="pull-right">Video Interview for Practice</span>
+          </h3>
         </div>
 
         {!isPlayBackOpen && videoQuestions.isFetched &&
-        <div className="row">
-          <div className="col-md-6 question-box">
-            <p className="question-header">Practice Question {currentQuestion + 1} of 5</p>
-            <span className="practice-question">{question}</span>
-          </div>
-
-          <div className="col-md-6">
-            <p className="question-time">
-              Question Time: {videoSettings.value['video_interview_response_time']} second(s)
-            </p>
-            <RecordCtl
-              onStop={this.onStopRecord}
-              onStart={this.onStartRecord}
-              countStop={isPlayBackOpen || isStopped}
-              prep={videoSettings.value['video_interview_prep_countdown']}
-              response={videoSettings.value['video_interview_response_time']}
-            />
-          </div>
-          <div className="col-sm-12 video_container">
-            <Webcam height="300" width="700" style={{marginLeft: '-150px', marginTop: '5px'}}/>
-            <div className="audio-box">
-              <AudioMeter/>
+        <React.Fragment>
+          <div className="row">
+            <div className="col-sm-12 question-box">
+                <p className="question-header text-center">{question}</p>
             </div>
           </div>
-        </div>
-        }
-
-        { !isPlayBackOpen && isStopped &&
-          <div className="col-md-12 playbackbtn-wrapper">
-            <RaisedButton
-              label="Play Back"
-              className="btn-playback"
-              onClick={this.openPlayBack}
-              primary={true}
-            />
+          <div className="video-box flex-column">
+            <div className="row video-status">
+              <div className="col-sm-6">
+                <p>Question Time: <b>{remainingTime[1]} second(s)</b></p>
+              </div>
+              <div className="col-sm-6">
+                <p>Preparation Limit: <b>{remainingTime[0]} second(s)</b></p>
+              </div>
+            </div>
+            <div className="row video-webcam"> 
+              <div className="audio-box">
+                <AudioMeter/>
+              </div>
+              <Webcam height="420" width="100%"/>
+              <div className="video-progress">
+                <RecordCtl
+                  remaining={remainingTime[timePos]}
+                  total={waitingTime[timePos]}
+                />
+              </div>
+            </div>
           </div>
+        </React.Fragment>
+        }
+        { !isPlayBackOpen && 
+          (<div className="col-md-12 playbackbtn-wrapper">
+            { (!isPlaying && !isStopped && (
+                <RaisedButton
+                  label={'Start Recording'}
+                  className="btn-start-start"
+                  onClick={this.onStartRecord}
+                  primary={true}
+                />
+              ))
+            }
+            {
+              ((isPlaying && !isStopped) && (
+                <RaisedButton
+                  label={'Stop Recording'}
+                  className="btn-start-stop"
+                  onClick={this.onStopRecord}
+                />
+              ))
+            }
+            {isStopped &&
+              <RaisedButton
+                label="Play Back"
+                className="btn-playback"
+                onClick={this.openPlayBack}
+                primary={true}
+              />
+            }
+          </div>)
         }
 
         {isPlayBackOpen &&  // Show playback.
-        <VideoPlayBack
-          currentQuestion={currentQuestion}
-          onNext={this.onNextQuestion}
-          onBack={this.onBack}
-        />
+          <VideoPlayBack
+            currentQuestion={currentQuestion}
+            onNext={this.onNextQuestion}
+            onBack={this.onBack}
+          />
         }
       </div>) : (<div className="video-black">
         <Dialog
